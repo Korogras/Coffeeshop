0a29cbeddc711b4fbfae64dc8f228f3ec57ad349
Korogras <brandon_faubion@yahoo.com>
2023-05-03 21:12:13 -0500

    printed combined price and made array for seasonal drinks

--- index.js
+++ index.js
@@ -14,4 +14,22 @@ const evenNumber = (item) => {
   return item.price % 2 === 0
 }
 const evenDrinks = (coffeeData.filter(evenNumber));
-console.log(evenDrinks)
+console.log(evenDrinks);
+
+const allPrices = (item) => {
+  return item.price
+}
+const priceArray = coffeeData.map(allPrices);
+console.log(priceArray);
+
+const sumTotalCost = (accumulator, currentValue) => {
+  return accumulator + currentValue;
+}
+const totalPrice = priceArray.reduce(sumTotalCost);
+console.log(totalPrice)
+
+const seasonalDrinks = (item) => {
+  return item.seasonal === true
+}
+const seasonalDrinksList = (coffeeData.filter(seasonalDrinks))
+console.log(seasonalDrinksList)
