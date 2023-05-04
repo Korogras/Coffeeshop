e0b1f7a2ae08941efd8647152c59be8114d70d23
Korogras <brandon_faubion@yahoo.com>
2023-05-03 19:23:24 -0500

    printed an array of every drink on menu

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const coffeedata = require ("./coffee_data");
+const coffeeData = require ("./coffee_data");
+const printName = (item) => {
+  return item.name
+}
+console.log(coffeeData.map(printName))
