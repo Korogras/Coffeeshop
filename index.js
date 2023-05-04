d228b7d2d923d2f048792e7e9736129d1eee4857
Korogras <brandon_faubion@yahoo.com>
2023-05-03 19:43:23 -0500

    printed an array for drinks 5 and under, and even number drinks

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const printName = (item) => {
   return item.name
 }
 console.log(coffeeData.map(printName))
+
+const fiveAndUnder = (item) => {
+  return item.price <= 5;
+}
+const drinksFiveAndUnder = (coffeeData.filter(fiveAndUnder));
+console.log(drinksFiveAndUnder)
+
+const evenNumber = (item) => {
+  return item.price % 2 === 0
+}
+const evenDrinks = (coffeeData.filter(evenNumber));
+console.log(evenDrinks)
